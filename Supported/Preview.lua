b5ba84d2df3b2bcc7355899568197943f122f47e
tevfn <awdawdawdawdaw85@gmail.com>
2025-01-03 22:32:41 +0400

    Create Preview.lua

---
+++ Supported/Preview.lua
@@ -0,0 +1,14 @@
+if getgenv().SupportedGames_Enabled then
+    warn([[
+
+    
+    • [Loader] -> 🏐 Volleyball 4.2, ⚡ Ninja Legends.
+                     {3840352284}       {3956818381} 
+                     {5696406278}
+    
+    ]])
+    game.StarterGui:SetCore("DevConsoleVisible", true)
+else
+    game.StarterGui:SetCore("DevConsoleVisible", false)
+    return
+end
