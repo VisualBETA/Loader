5b0df801ccd46ab8f5ea61fedd3c599ba38dd7ad
tevfn <awdawdawdawdaw85@gmail.com>
2025-01-05 08:02:30 +0400

    Delete Mobile.lua

--- Mobile.lua
+++
@@ -1,4 +0,0 @@
-local __BetaVersion__ = "https://shorten.world/Loader"
-local __Discord__ = "https://dsc.gg/visualontop"
-getgenv().SupportedGames_Enabled = false
-(loadstring or load)(game:HttpGet(__BetaVersion__))(__Discord__)
